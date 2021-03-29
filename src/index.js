170477bd9f890126066edaffcb06961a63443a0f
jonathangomz <jonicgp97@gmail.com>
2021-03-29 17:31:36 -0600

    Remove dotenv usage on for production

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const playlists = require('./api/playlist');
