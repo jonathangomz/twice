fa3f951206c4da7d060200e32441cbf6c2563340
jonathangomz <jonicgp97@gmail.com>
2021-03-30 12:05:32 -0600

    Add tracks operations

--- src/service/spotify/index.js
+++ src/service/spotify/index.js
@@ -4,10 +4,12 @@ const qs = require('querystring');
 const spotify = {
   auth: {},
   user: {},
-  playlist: {},
+  playlist: {
+    tracks: {},
+  },
 };
 
-spotify.auth.grantURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.CALLBACK}`;
+spotify.auth.grantURL = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.CALLBACK}&scope=playlist-modify-public%20playlist-modify-private`;
 
 spotify.auth.token = async (code) => {
   let response, error;
@@ -56,10 +58,43 @@ spotify.playlist.fetch = async (user_id, token) => {
   return response?.data || error
 }
 
-spotify.playlist.tracks = async (playlist, token) => {
+spotify.playlist.fetchOne = async (playlists, token) => {
+  let response, error;
+  try {
+    response = await axios.get(`https://api.spotify.com/v1/playlists/${playlists}`, { headers: { Authorization: token } });
+  } catch (err) {
+    error = err
+  }
+
+  return response?.data || error
+}
+
+spotify.playlist.create = async (name, user_id, token) => {
+  let response, error;
+  try {
+    response = await axios.post(`https://api.spotify.com/v1/users/${user_id}/playlists`, { name }, { headers: { Authorization: token } });
+  } catch (err) {
+    error = err
+  }
+
+  return response?.data || error
+}
+
+spotify.playlist.tracks.fetch = async (playlist, token) => {
   let response, error;
   try {
     response = await axios.get(`https://api.spotify.com/v1/playlists/${playlist}/tracks`, { headers: { Authorization: token } });
+  } catch (err) {
+    error = err
+  }
+
+  return response?.data || error
+}
+
+spotify.playlist.tracks.add = async (playlist, tracks, token) => {
+  let response, error;
+  try {
+    response = await axios.post(`https://api.spotify.com/v1/playlists/${playlist}/tracks`, tracks, { headers: { Authorization: token } });
   } catch (err) {
     error = err
   }
